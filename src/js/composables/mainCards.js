61f531fd79e5eaceb608eaa5968ed5df0b4f477a
Dima <grangeld3@gmail.com>
2021-01-27 18:05:05 +0200

    set data

--- src/js/composables/mainCards.js
+++ src/js/composables/mainCards.js
@@ -1,9 +1,15 @@
 import api from '../components/apiService';
 import template from '../templates/mainCards.hbs';
+import Handlebars from 'handlebars';
 
 const ul = document.querySelector('.js-ul-film');
 
+Handlebars.registerHelper('loud', function (aString) {
+  if (aString === undefined) return '';
+  return aString.substring(0, 4);
+});
+
 api.fetchMovies().then(data => {
-  const render = template(data);
+  const render = template(data, Handlebars);
   ul.insertAdjacentHTML('beforeend', render);
 });
