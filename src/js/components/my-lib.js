0f1237ef736ac251c79d79b9c2efc38203173925
s1dexdev <s1dexdev@gmail.com>
2021-01-27 20:05:32 +0200

    added markup and styles

--- src/js/components/my-lib.js
+++ src/js/components/my-lib.js
@@ -1,8 +1,18 @@
 import apiService from './apiService.js';
 import myLibTemp from '../templates/my-library.hbs';
 
+const refs = {
+  libraryList: document.querySelector('.my-library__list'),
+};
+
+const key = '5c34acfe39a6372a620da68979c929b1';
+
 apiService.fetchMovies().then(data => {
-  document
-    .querySelector('.my-library__list')
-    .insertAdjacentHTML('beforeend', myLibTemp(data));
+  const markup = myLibTemp(data);
+
+  refs.libraryList.insertAdjacentHTML('beforeend', markup);
 });
+
+// fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${key}&language=en-US`)
+//   .then(data => data.json())
+//   .then(e => console.log(e));
