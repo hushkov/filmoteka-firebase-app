9cd75f677202338459a159ccdc709b5a21cc157a
Dima <grangeld3@gmail.com>
2021-01-27 20:18:39 +0200

    add .film-list

--- src/js/composables/mainCards.js
+++ src/js/composables/mainCards.js
@@ -3,10 +3,15 @@ import template from '../templates/mainCards.hbs';
 import Handlebars from 'handlebars';
 
 const ul = document.querySelector('.js-ul-film');
-
-Handlebars.registerHelper('loud', function (aString) {
+//Редактирования даты
+Handlebars.registerHelper('dataEdit', function (aString) {
   if (aString === undefined) return '';
   return aString.substring(0, 4);
+});
+//Добавить текст в вверхний регистр
+Handlebars.registerHelper('upper', function (aString) {
+  if (aString === undefined) return '';
+  return aString.toUpperCase();
 });
 
 api.fetchMovies().then(data => {
