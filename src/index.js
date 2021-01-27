df92766fc89ae2a14895bee01c5ec68b12c976a3
Stanislav Kazikin <kazikinstas@gmail.com>
2021-01-27 20:18:03 +0200

    single film template

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import './js/components/refs';
 import './js/components/apiService';
 import apiService from './js/components/apiService';
 
-// console.log();
-// const input = document.querySelector('.input');
-// input.addEventListener('input', handleInput);
+console.log();
+const input = document.querySelector('.input');
+input.addEventListener('input', handleInput);
 
-// function handleInput(e) {
-//   const searchQuery = e.target.value;
-//   apiService.query = searchQuery;
-//   apiService.fetchMovies().then(data => console.log(data));
-// }
+function handleInput(e) {
+  const searchQuery = e.target.value;
+  apiService.query = searchQuery;
+  apiService.fetchMovies().then(data => console.log(data));
+}
 
 // apiService.fetchMovies().then(data =>
 //   data.forEach(({ title, name }) => {
