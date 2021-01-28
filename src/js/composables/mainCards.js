d9e90652bdc4f1c11c9c4281bf2c83cb140d951b
Dima <grangeld3@gmail.com>
2021-01-28 22:32:05 +0200

    add pagination

--- src/js/composables/mainCards.js
+++ src/js/composables/mainCards.js
@@ -1,6 +1,9 @@
 import apiService from '../components/apiService';
 import template from '../templates/mainCards.hbs';
 import Handlebars from 'handlebars';
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css';
+// var template = require('tui-code-snippet/domUtil/template');
 
 const ul = document.querySelector('.js-ul-film');
 
@@ -19,4 +22,23 @@ Handlebars.registerHelper('upper', function (aString) {
 apiService.fetchMovies().then(data => {
   const render = template(data.results, Handlebars); //results Если не будет работать удалить
   ul.insertAdjacentHTML('beforeend', render);
+});
+
+const options = {
+  totalItems: 2000,
+  itemsPerPage: 4,
+  visiblePages: 5,
+  page: 1,
+  firstItemClassName: 'tui-first-child',
+  lastItemClassName: 'tui-last-child',
+};
+const pagination = new Pagination('tui-pagination-container', options);
+// pagination.movePageTo(10);
+
+pagination.on('afterMove', function (eventData) {
+  // alert('The current page is ' + eventData.page);
+  apiService.fetchMovies().then(data => {
+    const render = template(data.results, Handlebars); //results Если не будет работать удалить
+    ul.insertAdjacentHTML('beforeend', render);
+  });
 });
