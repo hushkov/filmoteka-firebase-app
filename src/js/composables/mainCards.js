7c0deb5b36b8cad68617645317dceb9915a75b3c
Dima <grangeld3@gmail.com>
2021-01-28 18:33:02 +0200

    styles for tablet

--- src/js/composables/mainCards.js
+++ src/js/composables/mainCards.js
@@ -1,4 +1,4 @@
-import api from '../components/apiService';
+import apiService from '../components/apiService';
 import template from '../templates/mainCards.hbs';
 import Handlebars from 'handlebars';
 
@@ -16,21 +16,7 @@ Handlebars.registerHelper('upper', function (aString) {
   return aString.toUpperCase();
 });
 
-api.fetchMovies().then(data => {
+apiService.fetchMovies().then(data => {
   const render = template(data.results, Handlebars); //results Если не будет работать удалить
   ul.insertAdjacentHTML('beforeend', render);
 });
-/*
-function genre_ids(search) {
-  fetch(
-    'https://api.themoviedb.org/3/genre/movie/list?api_key=5c34acfe39a6372a620da68979c929b1',
-  ).this(response => response.json());
-  // .this(respons => {
-  //   console.log(search);
-  //   return search.map(num => respons.find(obj => obj.id === num).name);
-  // });
-}*/
-
-fetch(
-  'https://api.themoviedb.org/3/genre/movie/list?api_key=5c34acfe39a6372a620da68979c929b1',
-).this(data => console.log(data.json()));
