97e9064f8569486613cef958190af4dced0d7ed8
Alexander Hushkov <hush.kov.dev@gmail.com>
2021-01-28 20:55:19 +0200

    few changes and merged

--- src/index.js
+++ src/index.js
@@ -3,26 +3,3 @@ import './js/components/refs';
 import './js/components/modal.js';
 import './js/components/apiService';
 import './js/components/handleSignup';
-// import apiService from './js/components/apiService';
-
-// console.log();
-const input = document.querySelector('.input');
-input.addEventListener('input', handleInput);
-
-// function handleInput(e) {
-//   const searchQuery = e.target.value;
-//   apiService.query = searchQuery;
-//   apiService.fetchMovies().then(data => console.log(data));
-// }
-
-// apiService.fetchMovies().then(({ results }) =>
-//   results.forEach(({ title, name }) => {
-//     if (title) {
-//       console.log(title);
-//     } else {
-//       console.log(name);
-//     }
-//   }),
-// );
-
-// console.log(apiService.getMoviesData());
