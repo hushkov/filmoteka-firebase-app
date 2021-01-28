c7c5e01f91e55b304ea4e7d2c65d676167566690
Alexander Hushkov <hush.kov.dev@gmail.com>
2021-01-28 13:23:00 +0200

    add realease date

--- src/js/components/apiService.js
+++ src/js/components/apiService.js
@@ -54,17 +54,19 @@ export default {
         totalPages = data.total_pages;
         totalResults = data.total_results;
 
-        // console.log(data);
         array = [...data.results];
         let str = '';
         array.forEach(e => {
           const obj = {};
-          // obj.pages = totalPages;
+
           obj.id = e.id;
           obj.popularity = e.popularity;
           obj.poster_path = e.poster_path;
-          obj.title = e.title;
-          obj.name = e.name;
+
+          obj.title = !e.title ? e.name : e.title;
+          obj.release_date = !e.release_date
+            ? e.first_air_date.substr(0, 4)
+            : e.release_date.substr(0, 4);
           str = '';
           let genreArray = [];
           [...e.genre_ids].forEach(number => {
