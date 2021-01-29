3fa1c539109f563382936c447c5482e4f1a3e477
Serhii Stanislav <serhiistanislav@gmail.com>
2021-01-29 15:21:36 +0200

    adds closing by Esc

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -5,13 +5,27 @@ const openModalBtnRef = document.querySelector('button[data-open-modal]');
 const modalTemplate = document.querySelector('#modal');
 
 const instance = basicLightbox.create(modalTemplate, {
-   onShow: instance => {
-      const closeModalBtnRef = instance.element().querySelector('button[data-close-modal]');
+   onShow(instance) {
+      const closeModalBtnRef = getCloseModalBtnRef(instance);
+      closeModalBtnRef.addEventListener('click', instance.close);
 
-      closeModalBtnRef.onclick = instance.close
+      window.addEventListener('keydown', closeModalOnEsc);
+   },
+   onClose(instance) {
+      const closeModalBtnRef = getCloseModalBtnRef(instance);
+      closeModalBtnRef.removeEventListener('click', instance.close);
    },
 });
 
 openModalBtnRef.addEventListener('click', instance.show);
 
-// ===
+function getCloseModalBtnRef(parent) {
+   return parent.element().querySelector('button[data-close-modal]')
+};
+
+function closeModalOnEsc(e) {
+   if (e.code === 'Escape') {
+      instance.close();
+      window.removeEventListener('keydown', closeModalOnEsc);
+   };
+};
