8b14bc2ea913d956008638adb61b9a4c07b7eeae
s1dexdev <s1dexdev@gmail.com>
2021-01-29 19:44:18 +0200

    update

--- src/js/components/my-lib.js
+++ src/js/components/my-lib.js
@@ -2,12 +2,27 @@ import apiService from './apiService.js';
 import myLibTemp from '../templates/my-library.hbs';
 
 const refs = {
+  homeBtn: document.querySelector('.link-home'),
+  myLibraryBtn: document.querySelector('.link-library'),
+  myLibrary: document.querySelector('.my-library'),
   libraryList: document.querySelector('.my-library__list'),
+  watchedBtn: document.querySelector('.watched-btn'),
+  queueBtn: document.querySelector('.queue-btn'),
 };
 
-apiService.getMoviesData().then(data => {
-  // слайс прописан временно(для теста)
-  const markup = myLibTemp(data.slice(0, 9));
+refs.myLibraryBtn.addEventListener('click', event => {
+  event.preventDefault();
 
-  refs.libraryList.insertAdjacentHTML('beforeend', markup);
+  refs.myLibrary.classList.remove('hidden');
+
+  apiService.getMoviesData().then(data => {
+    // слайс прописан временно(для теста)
+    const markup = myLibTemp(data.slice(0, 9));
+
+    refs.libraryList.insertAdjacentHTML('beforeend', markup);
+  });
+});
+
+refs.homeBtn.addEventListener('click', event => {
+  refs.myLibrary.classList.add('hidden');
 });
