8cab5449f04e0b02b91513610ecdf787c843734c
s1dexdev <s1dexdev@gmail.com>
2021-01-30 17:24:13 +0200

    update

--- src/js/components/my-lib.js
+++ src/js/components/my-lib.js
@@ -2,27 +2,29 @@ import apiService from './apiService.js';
 import myLibTemp from '../templates/my-library.hbs';
 
 const refs = {
+  mainSectionFilm: document.querySelector('.js-section-film'),
+  sectionLib: document.querySelector('.my-library'),
   homeBtn: document.querySelector('.link-home'),
   myLibraryBtn: document.querySelector('.link-library'),
-  myLibrary: document.querySelector('.my-library'),
   libraryList: document.querySelector('.my-library__list'),
-  watchedBtn: document.querySelector('.watched-btn'),
-  queueBtn: document.querySelector('.queue-btn'),
 };
 
-// refs.myLibraryBtn.addEventListener('click', event => {
-//   event.preventDefault();
+refs.myLibraryBtn.addEventListener('click', event => {
+  event.preventDefault();
 
-//   refs.myLibrary.classList.remove('hidden');
+  refs.mainSectionFilm.classList.add('hidden');
+  refs.sectionLib.classList.remove('hidden');
 
-// apiService.getMoviesData().then(data => {
-//   // слайс прописан временно(для теста)
-//   const markup = myLibTemp(data.slice(0, 9));
+  // Test -------->>>>
+  apiService.getMoviesData().then(data => {
+    const markup = myLibTemp(data.slice(0, 3));
+    // <<<----------
+    refs.libraryList.innerHTML = '';
+    refs.libraryList.insertAdjacentHTML('beforeend', markup);
+  });
+});
 
-//   refs.libraryList.insertAdjacentHTML('beforeend', markup);
-// });
-// });
-
-// refs.homeBtn.addEventListener('click', event => {
-//   refs.myLibrary.classList.add('hidden');
-// });
+refs.homeBtn.addEventListener('click', event => {
+  refs.sectionLib.classList.add('hidden');
+  refs.mainSectionFilm.classList.remove('hidden');
+});
