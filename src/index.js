73f03102b8ea021285c5a4b83b26a5a20f67362e
Alexander Hushkov <hush.kov.dev@gmail.com>
2021-01-30 21:10:53 +0200

    after footer changes

--- src/index.js
+++ src/index.js
@@ -2,29 +2,24 @@ import './main.scss';
 import './js/components/refs';
 import './js/components/header';
 import './js/components/scrollToTopBtn.js';
-import './js/components/handleSignup';
 import './js/components/my-lib';
 import './js/composables/mainCards';
 import './js/components/modal.js';
 import './js/components/apiService';
-
-import './js/components/handleSignup';
-
-import './js/components/handleLogin';
-import './js/components/handleLogout';
-import './js/components/handleFilmID';
-import './js/composables/getUser';
-import './js/components/getLibrary';
-
 import './js/composables/footerModal';
 
-
-
+// import './js/components/handleSignup';
+// import './js/components/handleLogin';
+// import './js/components/handleLogout';
+// import './js/components/handleFilmID';
+// import './js/composables/getUser';
+// import './js/components/getLibrary';
 //============================
 import './js/composables/useLogin';
 import './js/composables/useSignup';
 import './js/composables/useLogout';
 
+//============================
 import { displayStartPage } from './js/composables/mainCards.js';
 displayStartPage();
 
