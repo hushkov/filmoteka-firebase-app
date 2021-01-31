29ae9a3220c7298810bacb987f95b02414facd7e
Alexander Hushkov <hush.kov.dev@gmail.com>
2021-01-31 22:54:40 +0200

    everything is good yet...

--- src/index.js
+++ src/index.js
@@ -8,25 +8,25 @@ import './js/composables/mainCards';
 import './js/components/apiService';
 import './js/composables/footerModal';
 import './js/components/singleFilmMarkup';
-
 import './js/components/spinnerOnOff';
-//===========================
+
+import { displayStartPage } from './js/composables/mainCards.js';
+displayStartPage();
+
+//=====================================
 // import './js/components/handleSignup';
 // import './js/components/handleLogin';
 // import './js/components/handleLogout';
 // import './js/components/handleFilmID';
 // import './js/composables/getUser';
 // import './js/components/getLibrary';
-//============================
+//=====================================
 import './js/composables/useLogin';
 import './js/composables/useSignup';
 import './js/composables/useLogout';
 
-//============================
-import { displayStartPage } from './js/composables/mainCards.js';
-displayStartPage();
-
-// apiService.query = 'wonder';
-
-// const show = apiService.getMoviesData();
-// console.log(show);
+//=====================================
+// import './js/route.js';
+// import './js/router.js';
+// import './js/app.js';
+//=====================================
