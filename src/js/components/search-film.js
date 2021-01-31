810fc698fef90fc72103b87731390a79d6e5027d
evgenii-ivanov87 <66361345+evgenii-ivanov87@users.noreply.github.com>
2021-01-31 23:56:59 +0200

    Fix3

--- src/js/components/search-film.js
+++ src/js/components/search-film.js
@@ -1,28 +1,62 @@
-import apiService from './apiService';
+import ImageApiService from './apiService';
+// import photoCard from '../templates/photoCard.hbs';
 
-const refs = {
-    form: document.querySelector('.search-form')
-    
+
+const formSearch = document.querySelector('#search-form');
+const articlesContainer = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('[data-action="load-more"]');
+const noResultMessage = document.querySelector('.notification');
+
+formSearch.addEventListener('submit', onSearch);
+loadMoreBtn.addEventListener('click', onLoadMore);
+articlesContainer.addEventListener('click', onOpenModal);
+
+const imageApiService = new ImageApiService();
+
+async function onSearch(event) {
+  event.preventDefault();
+
+  try {
+    noResultMessage.classList.add('is-hidden');
+    const inputSearchValue = event.currentTarget.elements.query.value;
+    imageApiService.query = inputSearchValue;
+
+    loadMoreBtn.classList.add('is-hidden');
+
+    imageApiService.resetPage();
+    clearArticlesContainer();
+    const response = await imageApiService.fetchImages();
+
+    if (response.length === 0) {
+      noResultMessage.classList.remove('is-hidden');
+    } else if (response.length > 0) {
+      appendArticlesMarkup(response);
+      loadMoreBtn.classList.remove('is-hidden');
+    }
+    if (response.length < 12) {
+      loadMoreBtn.classList.add('is-hidden');
+    }
+  } catch (error) {
+    console.log('Ошибка');
+  }
 }
-console.dir(refs.form)
 
-refs.form.addEventListener('submit', newFilms)
+async function onLoadMore() {
+  try {
+    const response = await imageApiService.fetchImages();
+    console.log(response);
 
-const newFilms = event => {
-  event.preventDefault()
-  console.log("event: ", event); 
+    appendArticlesMarkup(response);
+    scrollToElement();
+  } catch (error) {
+    console.log('Ошибка');
+  }
+}
 
-  console.log("event type: ", event.type); 
-  console.log("this: ", this); 
-  console.log("target: ", event.target);
-};
+function appendArticlesMarkup(articles) {
+  articlesContainer.insertAdjacentHTML('beforeend', photoCard(articles));
+}
 
-// apiService.fetchMovies().then(({results}) =>
-//   results.forEach(({ title, name }) => {
-//     if (title) {
-//       console.log(title);
-//     } else {
-//       console.log(name);
-//     }
-//   }),
-// );
+function clearArticlesContainer() {
+  articlesContainer.innerHTML = '';
+}
