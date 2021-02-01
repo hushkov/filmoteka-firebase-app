3c3b018fec0b3af89ce568bef104d5a1fd6b4312
evgenii-ivanov87 <66361345+evgenii-ivanov87@users.noreply.github.com>
2021-02-01 08:35:32 +0200

    поиск
    переделана логика поиска

--- src/js/components/search-film.js
+++ src/js/components/search-film.js
@@ -1,62 +1,62 @@
-import ImageApiService from './apiService';
+import fetchMovies from './apiService';
 // import photoCard from '../templates/photoCard.hbs';
 
 
 const formSearch = document.querySelector('#search-form');
-const articlesContainer = document.querySelector('.gallery');
-const loadMoreBtn = document.querySelector('[data-action="load-more"]');
-const noResultMessage = document.querySelector('.notification');
+// const articlesContainer = document.querySelector('.gallery');
+// const loadMoreBtn = document.querySelector('[data-action="load-more"]');
+// const noResultMessage = document.querySelector('.notification');
 
 formSearch.addEventListener('submit', onSearch);
-loadMoreBtn.addEventListener('click', onLoadMore);
-articlesContainer.addEventListener('click', onOpenModal);
+// loadMoreBtn.addEventListener('click', onLoadMore);
+// articlesContainer.addEventListener('click', onOpenModal);
 
-const imageApiService = new ImageApiService();
+// const apiService = new fetchMovies();
 
 async function onSearch(event) {
   event.preventDefault();
+  
+//   try {
+//     noResultMessage.classList.add('is-hidden');
+//     const inputSearchValue = event.currentTarget.elements.query.value;
+//     fetchMovies.query = inputSearchValue;
 
-  try {
-    noResultMessage.classList.add('is-hidden');
-    const inputSearchValue = event.currentTarget.elements.query.value;
-    imageApiService.query = inputSearchValue;
+//     loadMoreBtn.classList.add('is-hidden');
 
-    loadMoreBtn.classList.add('is-hidden');
+//     fetchMovies.resetPage();
+//     clearArticlesContainer();
+//     const response = await apiService.fetchMovies();
 
-    imageApiService.resetPage();
-    clearArticlesContainer();
-    const response = await imageApiService.fetchImages();
+//     if (response.length === 0) {
+//       noResultMessage.classList.remove('is-hidden');
+//     } else if (response.length > 0) {
+//       appendArticlesMarkup(response);
+//       loadMoreBtn.classList.remove('is-hidden');
+//     }
+//     if (response.length < 12) {
+//       loadMoreBtn.classList.add('is-hidden');
+//     }
+//   } catch (error) {
+//     console.log('Ошибка');
+//   }
+// }
 
-    if (response.length === 0) {
-      noResultMessage.classList.remove('is-hidden');
-    } else if (response.length > 0) {
-      appendArticlesMarkup(response);
-      loadMoreBtn.classList.remove('is-hidden');
-    }
-    if (response.length < 12) {
-      loadMoreBtn.classList.add('is-hidden');
-    }
-  } catch (error) {
-    console.log('Ошибка');
-  }
-}
+// async function onLoadMore() {
+//   try {
+//     const response = await imageApiService.fetchImages();
+//     console.log(response);
 
-async function onLoadMore() {
-  try {
-    const response = await imageApiService.fetchImages();
-    console.log(response);
+//     appendArticlesMarkup(response);
+//     scrollToElement();
+//   } catch (error) {
+//     console.log('Ошибка');
+//   }
+// }
 
-    appendArticlesMarkup(response);
-    scrollToElement();
-  } catch (error) {
-    console.log('Ошибка');
-  }
-}
+// function appendArticlesMarkup(articles) {
+//   articlesContainer.insertAdjacentHTML('beforeend', photoCard(articles));
+// }
 
-function appendArticlesMarkup(articles) {
-  articlesContainer.insertAdjacentHTML('beforeend', photoCard(articles));
-}
-
-function clearArticlesContainer() {
-  articlesContainer.innerHTML = '';
-}
+// function clearArticlesContainer() {
+//   articlesContainer.innerHTML = '';
+// }
