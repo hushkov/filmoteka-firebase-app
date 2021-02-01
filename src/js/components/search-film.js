3c11cfb97d097a691e6d4b32a2669939e8b29935
evgenii-ivanov87 <66361345+evgenii-ivanov87@users.noreply.github.com>
2021-02-01 22:52:10 +0200

    Логика поиска
    Переписал логику поиска

--- src/js/components/search-film.js
+++ src/js/components/search-film.js
@@ -1,62 +1,29 @@
-import fetchMovies from './apiService';
-// import photoCard from '../templates/photoCard.hbs';
+import apiService from './apiService.js'
+import myLibTemp from '../templates/my-library.hbs';
+// import loaderToggle from './loader';
+
+const refs = {
+  searchForm: document.querySelector('#search-form'),
+  mainSectionFilm: document.querySelector('.js-section-film'),
+  sectionLib: document.querySelector('.my-library'),
+  libraryList: document.querySelector('.my-library__list'),
+}
 
 
-const formSearch = document.querySelector('#search-form');
-// const articlesContainer = document.querySelector('.gallery');
-// const loadMoreBtn = document.querySelector('[data-action="load-more"]');
-// const noResultMessage = document.querySelector('.notification');
 
-formSearch.addEventListener('submit', onSearch);
-// loadMoreBtn.addEventListener('click', onLoadMore);
-// articlesContainer.addEventListener('click', onOpenModal);
+refs.searchForm.addEventListener('submit', onSearch);
 
-// const apiService = new fetchMovies();
-
-async function onSearch(event) {
+function onSearch(event) {
   event.preventDefault();
   
-//   try {
-//     noResultMessage.classList.add('is-hidden');
-//     const inputSearchValue = event.currentTarget.elements.query.value;
-//     fetchMovies.query = inputSearchValue;
-
-//     loadMoreBtn.classList.add('is-hidden');
-
-//     fetchMovies.resetPage();
-//     clearArticlesContainer();
-//     const response = await apiService.fetchMovies();
-
-//     if (response.length === 0) {
-//       noResultMessage.classList.remove('is-hidden');
-//     } else if (response.length > 0) {
-//       appendArticlesMarkup(response);
-//       loadMoreBtn.classList.remove('is-hidden');
-//     }
-//     if (response.length < 12) {
-//       loadMoreBtn.classList.add('is-hidden');
-//     }
-//   } catch (error) {
-//     console.log('Ошибка');
-//   }
-// }
-
-// async function onLoadMore() {
-//   try {
-//     const response = await imageApiService.fetchImages();
-//     console.log(response);
-
-//     appendArticlesMarkup(response);
-//     scrollToElement();
-//   } catch (error) {
-//     console.log('Ошибка');
-//   }
-// }
-
-// function appendArticlesMarkup(articles) {
-//   articlesContainer.insertAdjacentHTML('beforeend', photoCard(articles));
-// }
-
-// function clearArticlesContainer() {
-//   articlesContainer.innerHTML = '';
-// }
+   refs.mainSectionFilm.classList.add('hidden');
+  refs.sectionLib.classList.remove('hidden');
+  apiService.query = event.currentTarget.elements.query.value ;
+   
+  apiService.getMoviesData().then(data => {
+    const markup = myLibTemp(data.slice(0, 3));
+  })
+    refs.libraryList.innerHTML = '';
+    refs.libraryList.insertAdjacentHTML('beforeend', markup);
+  
+};
