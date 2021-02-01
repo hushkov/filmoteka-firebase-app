18192ebb02f4d6297387b04d2db2fb3517ad5540
Serhii Stanislav <serhiistanislav@gmail.com>
2021-02-01 19:50:45 +0200

    adds a switch instead of an if to the onOpenModal function

--- src/js/components/modalHandler.js
+++ src/js/components/modalHandler.js
@@ -1,104 +1,114 @@
-// import filmMarkup from '../templates/modal-film-markup.hbs';
 import signupMarkup from '../templates/modal-signup-markup.hbs';
 import loginMarkup from '../templates/modal-login-markup.hbs';
 import footerMarkup from '../templates/modal-footer-markup.hbs';
 import filmMarkup from '../templates/modal-film-markup.hbs';
+import rfs from './modalRefs.js';
 import refs from './refs';
 
-// // ---
-
-// import filmByID from './singleFilmMarkup.js';
-
-// // ---
-
-const openFilmModalBtn = document.querySelector('.js-section-film');
-const openSignupModalBtn = document.querySelector('button[data-open="signup"]');
-const openLoginModalBtn = document.querySelector('button[data-open="login"]');
-const openFooterModalBtn = document.querySelector('a[data-open="footer"]');
-const modalContentRef = document.querySelector('.modal-content');
-const closeModalBtn = document.querySelector('button[data-close-modal]');
-const backdropRef = document.querySelector(".js-backdrop");
-
-// ---
-
-const trendList = document.querySelector('.js-ul-film');
-
-// ---
-
-openFilmModalBtn.addEventListener("click", onOpenModal);
-openSignupModalBtn.addEventListener("click", onOpenModal);
-openLoginModalBtn.addEventListener("click", onOpenModal);
-openFooterModalBtn.addEventListener("click", onOpenModal);
-closeModalBtn.addEventListener("click", onCloseModal);
-backdropRef.addEventListener("click", onBackdropClick);
-
-// ---
-
-trendList.addEventListener('click', onOpenModal);
-
-// ---
+rfs.openFilmModalBtn.addEventListener("click", onOpenModal);
+rfs.openSignupModalBtn.addEventListener("click", onOpenModal);
+rfs.openLoginModalBtn.addEventListener("click", onOpenModal);
+rfs.openFooterModalBtn.addEventListener("click", onOpenModal);
+rfs.closeModalBtn.addEventListener("click", onCloseModal);
+rfs.backdropRef.addEventListener("click", onBackdropClick);
 
 // Open 
-function onOpenModal(event) {
-   window.addEventListener("keydown", onPressEscape);
-   event.preventDefault();
-   // const dataSet = event.target.dataset;
-   const targetID = event.target.dataset.id;
-   const dataOpen = event.target.dataset.open
+function onOpenModal(e) {
+   e.preventDefault();
+
+   const targetID = e.target.dataset.id;
+   const dataOpen = e.target.dataset.open;
    const preferMovie = refs.currentMoviesList.find(({ id }) => targetID == id);
 
-   if (dataOpen === "signup") {
-      document.body.classList.add("show-modal-signup");
-      updateModalMarkup(signupMarkup);
-   } else if (dataOpen === "login") {
-      document.body.classList.add("show-modal-signup");
-      updateModalMarkup(loginMarkup);
-   } else if (dataOpen === "film") {
-      document.body.classList.add("show-modal-film");
-      updateModalMarkup(filmMarkup, preferMovie, dataOpen);
-   } else if (dataOpen === "footer") {
-      document.body.classList.add("show-modal-footer");
-      updateModalMarkup(footerMarkup);
-   }
+   switch (dataOpen) {
+      case "signup":
+         rfs.bodyClass.add("show-modal-signup");
+         updateModalMarkup(signupMarkup);
+      break;
+      case "login":
+         rfs.bodyClass.add("show-modal-signup");
+         updateModalMarkup(loginMarkup);
+      break;
+      case "film":
+         rfs.bodyClass.add("show-modal-film");
+         updateModalMarkup(filmMarkup, preferMovie, dataOpen);
+      break;
+      case "footer":
+         rfs.bodyClass.add("show-modal-footer");
+         updateModalMarkup(footerMarkup);
+      break;
+   };
+
+   window.addEventListener("keydown", onPressEscape);
+   rfs.closeModal.classList.remove('hidden');
 };
 
+// Update Markup
 function updateModalMarkup(fn, data, dataOpen) {
-   if (dataOpen === "film") {
-      // const markup = fn(data);
-      modalContentRef.innerHTML = fn(data);
-   } else {
-      modalContentRef.innerHTML = fn();
-   }
-   
-}
+   dataOpen === "film"
+      ? rfs.modalContentRef.innerHTML = fn(data)
+      : rfs.modalContentRef.innerHTML = fn();   
+};
+
+// Close by backdrop
+function onBackdropClick(e) {
+   if (e.target === e.currentTarget) {
+      onCloseModal();
+   };
+};
+
+// Close by Esc
+function onPressEscape(e) {
+   if (e.code === "Escape") {
+      onCloseModal();
+   };
+};
+
+// Close Modal
+function onCloseModal() {
+   rfs.bodyClass.remove("show-modal-film");
+   rfs.bodyClass.remove("show-modal-signup");
+   rfs.bodyClass.remove("show-modal-footer");
+
+   rfs.closeModal.classList.add('hidden');
+   rfs.modalContentRef.innerHTML = "";
+   window.removeEventListener("keydown", onPressEscape);
+};
+
+
+
+
+// === Del --v
+
+// if (dataOpen === "film") {
+//    rfs.modalContentRef.innerHTML = fn(data);
+// } else {
+//    rfs.modalContentRef.innerHTML = fn();
+// }
+
+//    if (dataOpen === "signup") {
+//       document.body.classList.add("show-modal-signup");
+//       updateModalMarkup(signupMarkup);
+//    } else if (dataOpen === "login") {
+//       document.body.classList.add("show-modal-signup");
+//       updateModalMarkup(loginMarkup);
+//    } else if (dataOpen === "film") {
+//       document.body.classList.add("show-modal-film");
+//       updateModalMarkup(filmMarkup, preferMovie, dataOpen);
+//    } else if (dataOpen === "footer") {
+//       document.body.classList.add("show-modal-footer");
+//       updateModalMarkup(footerMarkup);
+//    }
+
+// --
 
 // function updateMarkup(data) {
 //    const markup = singleFilmTpl(data);
 //    modalContentRef.innerHTML = markup;
 // }
 
-// Close by backdrop
-function onBackdropClick(event) {
-   if (event.target === event.currentTarget) {
-      onCloseModal();
-   }
-};
-
-// Close by Esc
-function onPressEscape(event) {
-   if (event.code === "Escape") {
-      onCloseModal();
-   }
-};
-
-// Close Modal
-function onCloseModal() {
-   window.removeEventListener("keydown", onPressEscape);
-   document.body.classList.remove("show-modal-film");
-   document.body.classList.remove("show-modal-signup");
-   document.body.classList.remove("show-modal-footer");
-};
-
-export default modalContentRef;
+// --
 
 // modalContentRef.insertAdjacentHTML = apiService.getMoviesData().then(data => filmMarkup(data[0]));
+
+// -- 
