4b6124fabff0bbe77ff85622f5e536b788476c12
s1dexdev <s1dexdev@gmail.com>
2021-02-05 20:15:52 +0200

    added pnotify

---
+++ src/js/components/pnotify.js
@@ -0,0 +1,53 @@
+import * as PNotify from '@pnotify/core/dist/PNotify.js';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+
+function showStackBarTop(type) {
+  if (typeof window.stackBarTop === 'undefined') {
+    window.stackBarTop = new PNotify.Stack({
+      modal: false,
+      dir1: 'down',
+      firstpos1: 25,
+      spacing1: 0,
+      push: 'top',
+      maxOpen: Infinity,
+    });
+  }
+  const opts = {
+    title: 'Over Here',
+    text: "Check me out. I'm in a different stack.",
+    addClass: 'stack-bar-top',
+    shadow: false,
+    width: '100%',
+    mouseReset: true,
+    animation: 'none',
+    delay: 1500,
+    sticker: false,
+    closer: false,
+    stack: window.stackBarTop,
+  };
+  switch (type) {
+    case 'error':
+      opts.title = 'Oh No';
+      opts.text = 'Watch out for that water tower!';
+      opts.type = 'error';
+      break;
+    case 'info':
+      opts.title = 'Breaking News';
+      opts.text = 'Have you met Ted?';
+      opts.type = 'info';
+      break;
+    case 'success':
+      opts.title = 'Good News Everyone';
+      opts.text = "I've invented a device that bites shiny metal asses.";
+      opts.type = 'success';
+      break;
+  }
+  PNotify.alert(opts);
+}
+
+export default showStackBarTop;
+
+// npm install --save-dev @pnotify/core
+
+// showStackBarTop('success or error or info');
